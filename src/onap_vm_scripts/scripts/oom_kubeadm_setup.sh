784144a4a6f0019e0d06760e67f16997acb8a444
davidplunkett <dp7642@att.com>
2019-05-17 19:34:16 +0000

    update helm, tiller and calico versions
    Signed-off-by: davidplunkett <dp7642@att.com>
    Change-Id: I78b7ec7ebb2608092c6bb4b48fcd771adff5eda7

--- src/onap_vm_scripts/scripts/oom_kubeadm_setup.sh
+++ src/onap_vm_scripts/scripts/oom_kubeadm_setup.sh
@@ -14,12 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-
-
-
-
 IDIR=$(pwd)
 STATUSFILE=$IDIR/status-log
 
@@ -27,13 +21,7 @@ HTTPPROXY=$(cat /opt/config/http_proxy.txt)
 HTTPSPROXY=$(cat /opt/config/https_proxy.txt)
 NOPROXY=$(cat /opt/config/no_proxy.txt)
 
-
-
 logger -s [0%] Deploying K8S cluster 2>> $STATUSFILE
-
-
-
-
 
 if [[ ! -z $HTTPPROXY ]]; then
     echo "Acquire::http::Proxy \"$HTTPPROXY\";" | tee -a /etc/apt/apt.conf
@@ -50,18 +38,15 @@ if [[ ! -z $HTTPPROXY ]]; then
 
 fi
 
-
 apt-get update && apt-get install -y apt-transport-https
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 
-cat <<EOF > /etc/apt/sources.list.d/kubiernetes.list  
-deb http://apt.kubernetes.io/ kubernetes-xenial main  
+cat <<EOF > /etc/apt/sources.list.d/kubiernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 apt-get update
-
-
 apt-get install -y docker.io
 
 if [[ ! -z $HTTPPROXY ]]; then
@@ -74,21 +59,8 @@ if [[ ! -z $HTTPPROXY ]]; then
 
 fi
 
-
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-
-
-
-
-
 kubeadm init --service-cidr=10.96.0.0/12 --pod-network-cidr=192.168.0.0/16
-
-
-
-
-#mkdir -p $HOME/.kube
-#sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 mkdir -p /root/.kube
 sudo cp -f /etc/kubernetes/admin.conf /root/.kube/config
@@ -97,36 +69,22 @@ sudo chown $(id -u):$(id -g) /root/.kube/config
 export KUBECONFIG=$KUBECONFIG:/root/.kube/config
 export HELM_HOME=/root/.helm/
 
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
-kubectl label nodes onap kubeadm.alpha.kubernetes.io/role=master
+sleep 30
 
-#Setup kubernetes network
-#wget https://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
-
-#sed -i -e 's/10.96.232.136/10.96.0.136/g' calico.yaml
-
-
-kubectl apply -f calico.yaml
-
-
-#Enable master node run pod 
+#Enable master node run pod
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-
-
-#wget http://storage.googleapis.com/kubernetes-helm/helm-v2.7.2-linux-amd64.tar.gz
-wget http://storage.googleapis.com/kubernetes-helm/helm-v2.7.2-linux-amd64.tar.gz
-tar -zxvf helm-v2.7.2-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
-
-
+curl -LO https://git.io/get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
 
 kubectl create serviceaccount --namespace kube-system tiller
-
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 
-helm init --service-account tiller 
-
+helm init --service-account tiller
 
 echo "Waiting for K8S cluster to be ready."
 logger -s [0%] Waiting for K8S cluster to be ready 2>> $STATUSFILE
@@ -134,12 +92,12 @@ logger -s [0%] Waiting for K8S cluster to be ready 2>> $STATUSFILE
 NUMLINE=$(kubectl get pods --all-namespaces | grep -c kube-system)
 READINESS=$(kubectl get pods --all-namespaces | grep -c "0/")
 
-while [ ! $NUMLINE -ge 11 ] || [ ! $READINESS -eq 0 ]; do
-    
+while [ ! $NUMLINE -ge 9 ] || [ ! $READINESS -eq 0 ]; do
+
     kubectl get pods --all-namespaces
 
     echo ==================================
-    echo $NUMLINE active pods, $READINESS are not ready.    
+    echo $NUMLINE active pods, $READINESS are not ready.
     sleep 5
     NUMLINE=$(kubectl get pods --all-namespaces | grep -c kube-system)
     READINESS=$(kubectl get pods --all-namespaces | grep -c "0/")
@@ -149,4 +107,3 @@ done
 echo "K8S cluster is alive."
 logger -s [0%] K8S cluster is alive.>> $STATUSFILE
 
-#nohup ./deploy.sh 2>&1 &
